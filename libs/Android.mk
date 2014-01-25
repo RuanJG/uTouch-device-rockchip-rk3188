2bd852c4b5b137f6c75452f4d96c91ae8035c87e
Cody.Xie <Cody.Xie@rock-chips.com>
2014-01-25 14:56:23 +0800

    libs/Android.mk : fix duplicate definition for multi product directories

--- libs/Android.mk
+++ libs/Android.mk
@@ -2,6 +2,8 @@ ifeq ($(strip $(TARGET_BOARD_PLATFORM_GPU)), mali400)
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)),rk3188)
+ifeq ($(strip $(TARGET_PRODUCT)),rk3188)
+
 ifeq ($(strip $(BOARD_USE_LCDC_COMPOSER)),true)
 
     include $(CLEAR_VARS)
@@ -28,6 +30,8 @@ else
     LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
     include $(BUILD_MULTI_PREBUILT)
+
+endif
 
 endif
 endif
