74dddca8f318256e9615279e5a24537c1d0c7467
WengBaijie <wengbaijie@t-chip.com.cn>
2014-02-08 14:13:10 +0800

    Revert "libs/Android.mk : fix duplicate definition for multi product directories"
    This reverts commit 2bd852c4b5b137f6c75452f4d96c91ae8035c87e.

--- libs/Android.mk
+++ libs/Android.mk
@@ -2,8 +2,6 @@ ifeq ($(strip $(TARGET_BOARD_PLATFORM_GPU)), mali400)
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)),rk3188)
-ifeq ($(strip $(TARGET_PRODUCT)),rk3188)
-
 ifeq ($(strip $(BOARD_USE_LCDC_COMPOSER)),true)
 
     include $(CLEAR_VARS)
@@ -30,8 +28,6 @@ else
     LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
     include $(BUILD_MULTI_PREBUILT)
-
-endif
 
 endif
 endif
